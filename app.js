8925853bf5e0ec4aca94a8e96b0bf682fbbf0531
SoloTrane1958 <talktomatyaboutit@gmail.com>
2022-10-20 20:58:46 -0300

    final mercado liebre

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3031, () => {
-    console.log('Servidor iniciado')
-});
+const port = process.env.PORT || 3001;
+app.listen(port, () =>console.log(`Servidor corriendo en el puerto ${port}`));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
