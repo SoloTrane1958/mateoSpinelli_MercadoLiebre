5a71e678c92b4392ff2c4a42e06f77c7ab502352
SoloTrane1958 <talktomatyaboutit@gmail.com>
2022-10-20 23:24:11 -0300

    bajada final

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 3001;
-app.listen(port, () =>console.log(`Servidor corriendo en el puerto ${port}`));
+app.listen( process.env.PORT || 3000,()=>console.log('Servidor corriendo en el puerto 3000' ))
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
